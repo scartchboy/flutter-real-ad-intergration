407708772970800310383106db9619c6fc534590
shaurya <badrinath6024@gmail.com>
2021-01-01 04:47:01 -0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/realad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.realad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_admob/firebase_admob.dart';
+
+void main() => runApp(realap);
+
+class realap extends StatelessWidget {
+  const realap({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  Homepage({Key key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    BannerAd mybannerAd;
+
+    BannerAd buildBannerad() {
+      return BannerAd(
+          adUnitId: null,
+          size: AdSize.banner,
+          listener: (MobileAdEvent event) {
+            if (event == MobileAdEvent.loaded) {
+              mybannerAd..show();
+            }
+            print(event);
+          });
+    }
+
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("tuss"),
+          titleSpacing: 2.0,
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: null,
+            child: Text("banner ad"),
+          ),
+        ),
+      ),
+    );
+  }
+}
